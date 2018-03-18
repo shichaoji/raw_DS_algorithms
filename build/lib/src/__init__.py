40b943d2d941e875b6b4362607fb04cafce5c341
Shichao Ji <jshichao@vt.edu>
2018-03-18 12:15:00 -0400

    i2

---
+++ build/lib/src/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+#import sys
+#try:
+#    reload(sys)
+#    sys.setdefaultencoding("utf-8")
+#except:
+#    print('python3')
+
+from .base import *
+
+__version__ = '0.1'
+__license__ = 'MIT'
+
+#__all__ = ['string2second', 'extract_date', 'dfcat2n', 'dfcat2dummy', 'strtimeconv', 'showpic']
+
+def main():
+    print ('executing...')
+    #base_main()
+
---
+++ build/lib/src/norms.py
@@ -0,0 +1,39 @@
+class l1_regularization():
+    """ Regularization for Lasso Regression """
+    def __init__(self, alpha):
+        self.alpha = alpha
+    
+    def __call__(self, w):
+        return self.alpha * np.linalg.norm(w)
+
+    def grad(self, w):
+        return self.alpha * np.sign(w)
+
+    
+class l2_regularization():
+    """ Regularization for Ridge Regression """
+    def __init__(self, alpha):
+        self.alpha = alpha
+    
+    def __call__(self, w):
+        return self.alpha * 0.5 *  w.T.dot(w)
+
+    def grad(self, w):
+        return self.alpha * w    
+    
+class l1_l2_regularization():
+    """ Regularization for Elastic Net Regression """
+    def __init__(self, alpha, l1_ratio=0.5):
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
+
+    def __call__(self, w):
+        l1_contr = self.l1_ratio * np.linalg.norm(w)
+        l2_contr = (1 - self.l1_ratio) * 0.5 * w.T.dot(w) 
+        return self.alpha * (l1_contr + l2_contr)
+
+    def grad(self, w):
+        l1_contr = self.l1_ratio * np.sign(w)
+        l2_contr = (1 - self.l1_ratio) * w
+        return self.alpha * (l1_contr + l2_contr) 
+    
